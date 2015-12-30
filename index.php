f238db181679f8bafc12993b90bd5e2328a22d36
gujiaming123 <zjkkg125@163.com>
2015-12-30 13:28:09 +0800

    Create index.php
    网站的首页

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+<title>java文本单词提取器，上传文本页面</title>
+</head>
+
+<body>
+<h1><p align="center">php单词提取器,文本使用txt</p></h1>
+<form action="check.php" method="post"
+enctype="multipart/form-data" >
+<p align="center">
+<label for="file" >文本名:</label>
+<input type="file" name="file" id="file" /> <br />
+<br />
+<input type="submit" name="submit" value="上传文件" />
+</p>
+</form>
+
+<P align="center">&nbsp;<FONT color=#3b3b3b>Copyright &copy; 苏州大学文正学院13物联网 Powered By<FONT color=#5b5b5b> </FONT><A href="default.php?mod=c&amp;s=ss1183c98"><FONT color=#434343>zjkfly</FONT></A> </FONT></P>
+
+</body>
+</html>
